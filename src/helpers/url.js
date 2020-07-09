7628818fa674cd5f806d6a62d63f192f7fbe2428
oreste-abizera <oresteabizera11.com>
2020-07-09 12:52:40 +0200

    / problem fix for production

--- src/helpers/url.js
+++ src/helpers/url.js
@@ -1,5 +1,5 @@
 export const url =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5000"
-    : "https://upcrew-api.herokuapp.com/";
+    : "https://upcrew-api.herokuapp.com";
 export const defaultImg = `${url}/uploads/avatar.jpg`;
