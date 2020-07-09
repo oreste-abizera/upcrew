e1f22b1c9fb9a5c87cf3a79e10ebeb0fbe09ef9d
oreste-abizera <oresteabizera11.com>
2020-07-09 12:50:39 +0200

    development and production api's links

--- src/helpers/url.js
+++ src/helpers/url.js
@@ -1,2 +1,5 @@
-export const url = process.env.API_URL || "http://localhost:5000";
+export const url =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5000"
+    : "https://upcrew-api.herokuapp.com/";
 export const defaultImg = `${url}/uploads/avatar.jpg`;
